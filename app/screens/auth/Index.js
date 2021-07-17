de5a969fa70b56707aa48182022ac08a1f3e4f7c
zahidalidev <m.zahidalidev@gmail.com>
2021-07-17 16:49:25 +0500

    animated tabs

--- app/screens/auth/Index.js
+++ app/screens/auth/Index.js
@@ -1,12 +1,60 @@
 import React, { useState } from 'react';
-import { ActivityIndicator, StatusBar, StyleSheet, Text, View } from 'react-native';
+import {
+    ActivityIndicator, StatusBar, StyleSheet, Text, View, TouchableOpacity, Animated, ScrollView,
+    Image, Dimensions
+} from 'react-native';
 import { RFPercentage } from 'react-native-responsive-fontsize';
 
+// config
 import Colors from "../../config/Colors"
+
+const { width } = Dimensions.get("window");
 
 function Index(props) {
 
-    const [indicator, showIndicator] = useState(false)
+    const [indicator, showIndicator] = useState(false);
+    const [active, setActive] = useState(0);
+    const [xTabOne, setXTabOne] = useState(0);
+    const [xTabTwo, setXTabTwo] = useState(0);
+    const [translateX, setTranslateX] = useState(new Animated.Value(0));
+    const [translateXTabOne, setTranslateXTabOne] = useState(new Animated.Value(0));
+    const [translateXTabTwo, setTranslateXTabTwo] = useState(new Animated.Value(width));
+    const [translateY, setTranslateY] = useState(-1000);
+
+    const handleSlide = type => {
+        Animated.spring(translateX, {
+            toValue: type,
+            duration: 100,
+            useNativeDriver: true
+        }).start();
+        if (active === 0) {
+            Animated.parallel([
+                Animated.spring(translateXTabOne, {
+                    toValue: 0,
+                    duration: 100,
+                    useNativeDriver: true
+                }).start(),
+                Animated.spring(translateXTabTwo, {
+                    toValue: width,
+                    duration: 100,
+                    useNativeDriver: true
+                }).start()
+            ]);
+        } else {
+            Animated.parallel([
+                Animated.spring(translateXTabOne, {
+                    toValue: -width,
+                    duration: 100,
+                    useNativeDriver: true
+                }).start(),
+                Animated.spring(translateXTabTwo, {
+                    toValue: 0,
+                    duration: 100,
+                    useNativeDriver: true
+                }).start()
+            ]);
+        }
+    };
 
     return (
         <View style={styles.container}>
@@ -26,7 +74,38 @@ function Index(props) {
                 : <>
                     {/* Bottom Contaienr */}
                     <View style={{ marginTop: -RFPercentage(5), borderTopRightRadius: RFPercentage(5), borderTopLeftRadius: RFPercentage(5), backgroundColor: Colors.lightGrey, width: "100%", flex: 2, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} >
+                        <View style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
 
+                            {/* Tabs */}
+                            <View style={{ flexDirection: "row", marginTop: 40, marginBottom: 20, height: 36, position: "relative" }}>
+                                <Animated.View style={{ position: "absolute", width: "50%", height: "100%", top: 0, left: 0, backgroundColor: "#007aff", borderRadius: 4, transform: [{ translateX }] }} />
+                                <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", borderWidth: 1, borderColor: "#007aff", borderRadius: 4, borderRightWidth: 0, borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
+                                    onLayout={event => setXTabOne(event.nativeEvent.layout.x)} onPress={() => { setActive(0); handleSlide(xTabOne) }}
+                                >
+                                    <Text style={{ color: active === 0 ? "#fff" : "#007aff" }}>
+                                        Tab One
+                                    </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", borderWidth: 1, borderColor: "#007aff", borderRadius: 4, borderLeftWidth: 0, borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+                                    onLayout={event => { setXTabTwo(event.nativeEvent.layout.x) }} onPress={() => { setActive(1); handleSlide(xTabTwo) }}
+                                >
+                                    <Text style={{ color: active === 1 ? "#fff" : "#007aff" }}>
+                                        Tab Two
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+
+                            {/* Login and Sign Up Container */}
+                            <ScrollView>
+                                <Animated.View style={{ justifyContent: "center", alignItems: "center", transform: [{ translateX: translateXTabOne }] }} onLayout={event => setTranslateY(event.nativeEvent.layout.height)}>
+                                    <Text>Hi, I am a Tab 2 area</Text>
+                                </Animated.View>
+
+                                <Animated.View style={{ justifyContent: "center", alignItems: "center", transform: [{ translateX: translateXTabTwo }, { translateY: -translateY }] }}>
+                                    <Text>Hi, I am a cute dog</Text>
+                                </Animated.View>
+                            </ScrollView>
+                        </View>
 
                     </View>
                 </>
